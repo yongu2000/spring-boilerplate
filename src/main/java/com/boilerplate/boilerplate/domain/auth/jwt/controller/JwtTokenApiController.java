2c1505050e5282f08c2c567b4590f6e636c05395
Yong U Cho <yongu2000@naver.com>
2025-03-12 16:01:39 +0900

    style: 재발급 기능 json 리턴 잠시 삭제

--- src/main/java/com/boilerplate/boilerplate/domain/auth/jwt/controller/JwtTokenApiController.java
+++ src/main/java/com/boilerplate/boilerplate/domain/auth/jwt/controller/JwtTokenApiController.java
@@ -1,8 +1,6 @@
 package com.boilerplate.boilerplate.domain.auth.jwt.controller;
 
 import com.boilerplate.boilerplate.domain.auth.jwt.JwtProperties;
-import com.boilerplate.boilerplate.domain.auth.jwt.dto.ReissueAccessTokenRequest;
-import com.boilerplate.boilerplate.domain.auth.jwt.dto.ReissueAccessTokenResponse;
 import com.boilerplate.boilerplate.domain.auth.jwt.service.AccessTokenService;
 import com.boilerplate.boilerplate.domain.auth.jwt.service.RefreshTokenService;
 import com.boilerplate.boilerplate.global.utils.CookieUtil;
@@ -12,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,19 +21,19 @@ public class JwtTokenApiController {
     private final AccessTokenService accessTokenService;
     private final RefreshTokenService refreshTokenService;
     private final JwtProperties jwtProperties;
+    
+//    @PostMapping("/json")
+//    public ResponseEntity<ReissueAccessTokenResponse> reissueAccessTokenAtJson(
+//        @RequestBody ReissueAccessTokenRequest request) {
+//
+//        String newAccessToken = accessTokenService.createNewAccessToken(request.getRefreshToken());
+//        String newRefreshToken = refreshTokenService.createNewRefreshToken(request.getRefreshToken());
+//
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//            .body(new ReissueAccessTokenResponse(newAccessToken, newRefreshToken));
+//    }
 
-    @PostMapping("/json")
-    public ResponseEntity<ReissueAccessTokenResponse> reissueAccessTokenAtJson(
-        @RequestBody ReissueAccessTokenRequest request) {
-
-        String newAccessToken = accessTokenService.createNewAccessToken(request.getRefreshToken());
-        String newRefreshToken = refreshTokenService.createNewRefreshToken(request.getRefreshToken());
-
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(new ReissueAccessTokenResponse(newAccessToken, newRefreshToken));
-    }
-
-    @PostMapping("/header")
+    @PostMapping("/reissue")
     public ResponseEntity<?> reissueAccessTokenAtHeader(HttpServletRequest request,
         HttpServletResponse response) {
 
